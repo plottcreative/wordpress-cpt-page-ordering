49a3d93294e90f4227b8e3d83753296bf45715fa
Ewan Lockwood <ewan@plott.co.uk>
2025-11-10 16:32:34 +0000

    Bump v0.1.2

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-    'process.env': {},
+    'process.env': {}, // safe shim
   },
   build: {
     lib: {
